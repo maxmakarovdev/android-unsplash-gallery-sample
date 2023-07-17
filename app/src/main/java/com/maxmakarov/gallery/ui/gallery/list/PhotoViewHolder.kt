bd964c99fa9d560191f5220e14e70a2abf948434
Max Makarov <maksimmakarovdev@proton.me>
2023-07-17 15:40:19 +0400

    Added BlurHash placeholders support

--- app/src/main/java/com/maxmakarov/gallery/ui/gallery/list/PhotoViewHolder.kt
+++ app/src/main/java/com/maxmakarov/gallery/ui/gallery/list/PhotoViewHolder.kt
@@ -1,6 +1,6 @@
 package com.maxmakarov.gallery.ui.gallery.list
 
-import android.graphics.Color
+import android.graphics.drawable.BitmapDrawable
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,6 +10,8 @@ import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.maxmakarov.gallery.R
 import com.maxmakarov.gallery.model.UnsplashPhoto
+import com.maxmakarov.gallery.ui.utils.BlurHashDecoder
+
 
 //todo use ViewBinding as an argument for a convenience
 class PhotoViewHolder(view: View, callback: PhotoClickCallback) : RecyclerView.ViewHolder(view) {
@@ -34,8 +36,9 @@ class PhotoViewHolder(view: View, callback: PhotoClickCallback) : RecyclerView.V
             this.photo = photo
 
             photoView.aspectRatio = photo.height.toDouble() / photo.width.toDouble()
-            photoView.setBackgroundColor(Color.parseColor(photo.color))
+            val bitmap = BlurHashDecoder.decode(photo.blur_hash, 20, 20)
             photoView.load(photo.urls.regular) {
+                placeholder(BitmapDrawable(photoView.resources, bitmap))
                 crossfade(true)
             }
 
