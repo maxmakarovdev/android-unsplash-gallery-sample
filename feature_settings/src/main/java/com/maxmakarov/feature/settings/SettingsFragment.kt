2128530b8948a517c26550961ef69bcad305c60b
Max Makarov <maksimmakarovdev@proton.me>
2023-07-21 18:55:58 +0400

    Added a Lottie animation to the settings screen

--- feature_settings/src/main/java/com/maxmakarov/feature/settings/SettingsFragment.kt
+++ feature_settings/src/main/java/com/maxmakarov/feature/settings/SettingsFragment.kt
@@ -1,7 +1,12 @@
 package com.maxmakarov.feature.settings
 
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
+import androidx.core.view.postDelayed
+import androidx.transition.TransitionManager
 import com.maxmakarov.core.ui.BaseFragment
 import com.maxmakarov.feature.settings.databinding.SettingsFragmentBinding
 
@@ -9,5 +14,15 @@ class SettingsFragment : BaseFragment<SettingsFragmentBinding>() {
 
     override fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): SettingsFragmentBinding {
         return SettingsFragmentBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.apply {
+            binding.root.postDelayed(1000) {
+                TransitionManager.beginDelayedTransition(binding.root)
+                nothingHere.isVisible = true
+            }
+        }
     }
 }
