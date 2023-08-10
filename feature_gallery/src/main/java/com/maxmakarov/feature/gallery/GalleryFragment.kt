f23a1021a052aab2b737af17d2b7d994e561dd5f
Max Makarov <maksimmakarovdev@proton.me>
2023-08-10 20:34:00 +0400

    It's possible to clear the search field now

--- feature_gallery/src/main/java/com/maxmakarov/feature/gallery/GalleryFragment.kt
+++ feature_gallery/src/main/java/com/maxmakarov/feature/gallery/GalleryFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
+import androidx.core.view.isVisible
 import androidx.core.view.marginTop
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
@@ -75,6 +76,9 @@ class GalleryFragment : BaseGalleryFragment<GalleryFragmentBinding>() {
                 false
             }
         }
+        searchClear.setOnClickListener {
+            onQueryChanged(UiAction.Search(query = ""))
+        }
 
         viewLifecycleOwner.lifecycleScope.launch {
             uiState
@@ -84,6 +88,7 @@ class GalleryFragment : BaseGalleryFragment<GalleryFragmentBinding>() {
                 .collect {
                     search.setText(it)
                     search.setSelection(search.length())
+                    searchClear.isVisible = it.isNotEmpty()
                 }
         }
     }
